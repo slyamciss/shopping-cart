a48c8698c37086e775c04f22d2e1df7acfcdcf66
Souleymane Cisse <slyamciss@gmail.com>
2021-09-28 22:14:47 -0400

    Final touche

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
+import { BrowserRouter, Route } from "react-router-dom";
 import "./App.css";
+import Cart from "./components/Cart";
 import Header from "./components/Header";
+import Home from "./components/Home";
 
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-    </div>
+      <div>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/cart" exact>
+          <Cart />
+        </Route>
+      </div>
+    </BrowserRouter>
   );
 }
 
